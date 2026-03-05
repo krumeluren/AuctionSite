463f0b87d73dd253b4fe865bf47951e2f9f0cdd9
LQ <58175665+krumeluren@users.noreply.github.com>
2026-03-05 07:28:05 +0100

    awd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                 {/* show admin routes if user is admin */}
                 {user?.isAdmin && (
                     <div style={{ borderLeft: '1px solid #576574', paddingLeft: '15px' }}>
-                        <span style={{ color: '#f39c12', marginRight: '10px' }}>Admin Panel:</span>
+                        <span class="admin-text" style={{ marginRight: '10px' }}>Admin Panel:</span>
                         <Link to="/admin/auctions">Hantera Auktioner</Link>
                         <span style={{ margin: '0 10px' }}>|</span>
                         <Link to="/admin/users">Hantera Användare</Link>
@@ -33,7 +33,7 @@ const Navbar = () => {
                 {user ? (
                     <>
                         <span style={{ color: '#bdc3c7' }}>
-                            {user.username} {user.isAdmin && <span style={{ color: '#f39c12' }}>(Admin)</span>}
+                            {user.username} {user.isAdmin && <span class="admin-text">(Admin)</span>}
                         </span>
                         <Link to="/auctions/create" className="btn" style={{ padding: '6px 12px', fontSize: '1em' }}>Skapa Auktion</Link>
                         <Link to="/profile">Konto</Link>
